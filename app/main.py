08497f93078e28addbe07fcead0959b544d9591c
HyScript7 <hyscript7@seznam.cz>
2023-01-12 10:57:55 +0100

    Logging

--- app/main.py
+++ app/main.py
@@ -11,11 +11,16 @@
 # See LICENSE.txt for more information
 #
 import asyncio
+import logging
 import os
+import time
+from logging import handlers
+from sys import stdout
 
 import discord
-from discord.ext import commands
+import models
 from decouple import config
+from discord.ext import commands
 from pymongo import MongoClient
 
 __version__ = "1.0"
@@ -26,13 +31,13 @@ MONGO_USER = config("MONGO_USER", "root").strip()
 MONGO_PASS = config("MONGO_PASS", "root").strip()
 MONGO_HOST = config("MONGO_HOST", "127.0.0.1").strip()
 MONGO_PORT = config("MONGO_PORT", "27017").strip()
-MONGO_SRV = bool(config("MONGO_SRV", "False"))
+MONGO_SRV = config("MONGO_SRV", "False").lower() == "true"
 
 
 class suDatabase:
     def __init__(
         self,
-        MONGO_SRV: str,
+        MONGO_SRV: bool,
         MONGO_HOST: str,
         MONGO_PORT: str,
         MONGO_USER: str,
@@ -47,7 +52,9 @@ class suDatabase:
 
 class suBot(commands.Bot):
     def __init__(self) -> None:
-        intents = discord.Intents.all() # TODO: Change this in the first release version to really only enable what it needs
+        intents = (
+            discord.Intents.all()
+        )  # TODO: Change this in the first release version to really only enable what it needs
         super().__init__(PREFIX, intents=intents)
         self.Synced = False
         self.DB = suDatabase(MONGO_SRV, MONGO_HOST, MONGO_PORT, MONGO_USER, MONGO_PASS)
@@ -59,31 +66,69 @@ class suBot(commands.Bot):
             if i.startswith("__"):
                 continue
             Modules.append("modules." + i.strip(".py"))
-        for i in self.Modules:
-            print(f"Extension {i} is loading")
+        for i in Modules:
+            logging.info(f"Extension {i} is loading")
             try:
                 await self.load_extension(i)
-                print(f"Extension {i} has been loaded")
+                logging.info(f"Extension {i} has been loaded")
             except Exception as e:
-                print(f"Extension {i} has failed to load: {e}")
+                logging.error(f"Extension {i} has failed to load: {e}")
 
     async def on_ready(self):
         await bot.wait_until_ready()
         if not self.Synced:
             await bot.tree.sync()
             self.Synced = True
-            print("Commands synced!")
+            logging.info("Commands synced!")
 
 
 bot = suBot()
+
+# ? There definitely has to be a better way to do this
+# Create loggers & set levels
+discord_logger = logging.getLogger("discord")
+discord_logger.setLevel(logging.INFO)
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+logging.getLogger("discord.http").setLevel(logging.INFO)
+
+# Create formatter
+dt_fmt = "%d.%m.%Y %H:%M:%S"
+formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+)
+
+# Create handlers
+
+log_file = logging.handlers.RotatingFileHandler(
+    filename=f"./logs/ScriptUtils {time.ctime()}.log",
+    encoding="utf-8",
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=3,  # Rotate through 5 files
+)
+log_file.setFormatter(formatter)
+log_console = logging.StreamHandler(stdout)
+log_console.setFormatter(formatter)
+
+# Set handlers
+discord_logger.addHandler(log_file)
+root_logger.addHandler(log_file)
+root_logger.addHandler(log_console)
 
 if __name__ == "__main__":
     from sys import exit
 
     if (not len(TOKEN) or TOKEN.count(".") != 2) if type(TOKEN) is str else True:
+        logging.error("Invalid token: Token failed check")
         exit("Invalid bot token has been provided!")
-    
-    asyncio.run(bot.loadModules())
 
-    bot.run(TOKEN)
+    try:
+        asyncio.run(bot.loadModules())
+        bot.run(TOKEN, log_handler=None)
+    except KeyboardInterrupt:
+        logging.info("Received KeyboardInterrupt")
+    except Exception as e:
+        logging.error(f"Uncaught exception: {e}")
+        exit(e)
+    logging.info("Bot stopped, exiting")
     exit(0)
