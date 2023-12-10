46d8abc1bfe1ea970cd7583a73c88dfc59a5ad64
HyScript7 <hyscript7@seznam.cz>
2023-12-10 14:19:30 +0100

    🧵 Execute every command in it's own thread

--- src/main/java/me/hyscript7/scriptutils/commandmng/SlashCommandHandler.java
+++ src/main/java/me/hyscript7/scriptutils/commandmng/SlashCommandHandler.java
@@ -1,22 +1,31 @@
 package me.hyscript7.scriptutils.commandmng;
 
+import jakarta.annotation.PreDestroy;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 public class SlashCommandHandler extends ListenerAdapter {
+    private final Logger logger = LoggerFactory.getLogger(SlashCommandHandler.class);
 
     private final Map<String, SlashCommand> commands;
+    private final ExecutorService executorService;
 
     @Autowired
     public SlashCommandHandler(List<SlashCommand> slashCommands) {
         this.commands = new HashMap<>(slashCommands.size());
+        this.executorService = Executors.newCachedThreadPool();
+
         // Cache commands
         for (SlashCommand c :
                 slashCommands) {
@@ -30,7 +39,7 @@ public class SlashCommandHandler extends ListenerAdapter {
         if (event.isAcknowledged()) {
             return;
         }
-        executeCommand(event);
+        executorService.submit(() -> executeCommand(event));
     }
 
     private void executeCommand(SlashCommandInteractionEvent event) {
@@ -39,9 +48,23 @@ public class SlashCommandHandler extends ListenerAdapter {
         // Check if the command is registered
         SlashCommand command = commands.get(commandName);
         if (command == null) {
+            logger.info("Attempted to execute an unknown command!");
             event.reply("Unknown command").setEphemeral(true).queue();
             return;
         }
+        logger.info("Executing command " + commandName + "!");
         command.execute(event);
+    }
+
+    // Make sure we shut down the executor service when the app shuts down
+    public void shutdown() {
+        logger.info("Shutting down executor service!");
+        executorService.shutdown();
+    }
+
+    // Hook into ctrl + c and other shutdown events
+    @PreDestroy
+    public void onShutdown() {
+        shutdown();
     }
 }
